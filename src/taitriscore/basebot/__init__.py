561c3a298a2057d6f6813c26cae5ea63422c40aa
Belhal Karimi <belhal.karimi@gmail.com>
2024-05-24 10:59:45 +0200

    init basebot

--- src/taitriscore/basebot/__init__.py
+++ src/taitriscore/basebot/__init__.py
@@ -1,2 +1,11 @@
-# from taitriscore.basebot.hf_api import LLAMAAPI
-from taitriscore.basebot.openai_api import OpenAIAPI
+from taitriscore.config import CONFIG
+
+if CONFIG.platform == 'OPENAI':
+    from taitriscore.basebot.openai_api import OpenAIAPI
+elif CONFIG.platform == 'LLAMA':
+    from taitriscore.basebot.hf_api import LLAMAAPI
+elif CONFIG.platform == 'ANTHROPIC':
+    from taitriscore.basebot.anthropic_api import AnthropicAPI
+else:
+    raise ValueError(f"Unsupported platform type: {CONFIG.platform}")
+
