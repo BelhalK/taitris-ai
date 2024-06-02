7367faeeb5e96a186ce994eb1ea7c1a6da80b536
Belhal Karimi <belhal.karimi@gmail.com>
2024-06-02 12:42:48 +0200

    hf to llama api

--- src/taitriscore/llm.py
+++ src/taitriscore/llm.py
@@ -3,7 +3,7 @@ from taitriscore.config import CONFIG
 if CONFIG.platform == 'OPENAI':
     from taitriscore.basebot.openai_api import OpenAIAPI as LLM
 elif CONFIG.platform == 'LLAMAV2':
-    from taitriscore.basebot.hf_api import LLAMAAPI as LLM
+    from taitriscore.basebot.llama_api import LLAMAAPI as LLM
 elif CONFIG.platform == 'ANTHROPIC':
     from taitriscore.basebot.anthropic_api import AnthropicAPI as LLM
 else:
