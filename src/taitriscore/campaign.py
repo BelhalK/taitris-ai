f931349013bcd5838a220326b7c8a31cd9f9d4f6
Belhal Karimi <belhal.karimi@gmail.com>
2024-06-12 21:04:03 +0200

    maj

--- src/taitriscore/campaign.py
+++ src/taitriscore/campaign.py
@@ -18,14 +18,44 @@ class Campaign(BaseModel):
         self.environment.add_roles(roles=roles)
 
     def set_quota_seeding(self, quota_seeding):
+        """
+        Sets the maximum number of products seeding in the campaign.
+
+        Args:
+            quota_seeding (int): The maximum number of seedings allowed in the campaign.
+
+        Returns:
+            None
+        """
         CONFIG.max_quota_seeding = quota_seeding
         logger.info(f"Quota Seeding: {quota_seeding}.")
 
     def budget(self, budget):
+        """
+        Sets the maximum budget for the campaign.
+
+        Args:
+            budget (int): The maximum budget for the campaign.
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
         CONFIG.max_budget = budget
         logger.info(f"Budget: ${budget}.")
 
     def _check_balance(self):
+        """
+        Checks the balance of the campaign by comparing the total number of products to be seeded with the maximum quota allowed for seeding.
+
+        Raises:
+            ValueError: If the total number of products to be seeded exceeds the maximum quota allowed for seeding.
+
+        Returns:
+            None
+        """
         if CONFIG.total_seeding > CONFIG.max_quota_seeding:
             raise f"No More products to place -> Nb of Products placed: {CONFIG.total_seedings}"
 
