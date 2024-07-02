6f04b2b2982a98f76de9f08fd62ef49631a6ba48
Belhal Karimi <belhal.karimi@gmail.com>
2024-07-02 22:39:02 +0200

    clean path and constants

--- src/taitriscore/const.py
+++ src/taitriscore/const.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 
+custom_env="/Users/belhalkarimi/Desktop/Belhal/Tech/taitris/taitris-ai"
 
 def get_project_root():
+    """
+    Finds the project root by searching for .git, .project_root, or .gitignore
+    files or directories. Raises an exception if not found.
+    """
     current_path = Path.cwd()
     while True:
         if (
@@ -15,24 +20,26 @@ def get_project_root():
             raise Exception("Project root not found.")
         current_path = parent_path
 
-
 try:
     ENV_ROOT = get_project_root()
-    PROJECT_ROOT = get_project_root() / "src"
+    PROJECT_ROOT = ENV_ROOT / "src"
     DATA_PATH = PROJECT_ROOT / "data"
     WORKSPACE_ROOT = PROJECT_ROOT / "workspace"
     PROMPT_PATH = PROJECT_ROOT / "taitriscore/prompts"
     TMP = PROJECT_ROOT / "tmp"
     RESEARCH_PATH = DATA_PATH / "research"
-except:
-    ENV_ROOT = Path("/Users/belhalkarimi/Desktop/Belhal/Tech/taitris/taitris-ai")
-    PROJECT_ROOT = Path(
-        "/Users/belhalkarimi/Desktop/Belhal/Tech/taitris/taitris-ai/src"
-    )
+except Exception as e:
+    # Log the exception if needed
+    print(f"Error finding project root: {e}")
+    
+    # Default paths if project root is not found
+    ENV_ROOT = Path(custom_env)
+    PROJECT_ROOT = ENV_ROOT / "src"
     DATA_PATH = PROJECT_ROOT / "data"
     WORKSPACE_ROOT = PROJECT_ROOT / "workspace"
     PROMPT_PATH = PROJECT_ROOT / "taitriscore/prompts"
     TMP = PROJECT_ROOT / "tmp"
     RESEARCH_PATH = DATA_PATH / "research"
 
+# Time-to-live for memory in seconds
 MEM_TTL = 24 * 30 * 3600
